a0c3acee85ede28e3fb1c73f217c503c299b100b
2Anblo <1542988920@qq.com>
2023-09-06 20:46:03 +0800

    输入一些在cpp程序中

---
+++ 05putin/05putin/05input.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<string>         //cpp head file for string;
+using namespace std;
+int main()
+{
+	//int a = 0;
+	string str;
+	cin >> str;
+	cout << str << endl;
+	system("pause");
+	return 0;
+}
+/*//cpp ·ç¸ñ×Ö·û´®
+	string str = "hello niko";
+	cout << str << endl;
+	bool flag = false;
+	cout << !flag<< endl;
+	*/
