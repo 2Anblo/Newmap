991de938609835e3c0af29b49bd23b552436c5e1
2Anblo <1542988920@qq.com>
2023-09-08 11:46:27 +0800

    PPPPPPOINTER

---
+++ 13Pointer/13Pointer/pointer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	/*int a = 10;
+	int* p = &a;
+	cout << sizeof(p) << endl;*/
+	/*cout << sizeof(char*) << endl;
+	cout << sizeof(int*) << endl;
+	cout << sizeof(float*) << endl;
+	cout << sizeof(double*) << endl;*/
+	int* p = NULL;
+	*p = 100;
+	system("pause");
+	return 0;
+}
